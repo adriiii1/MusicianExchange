b8ec3701d363fbf891b25b1c80548015aebfd754
Adrián Pérez del Pulgar García <adriperezNMC@outlook.com>
2019-04-30 12:12:57 +0200

    Eh

--- app/src/main/java/com/adri/musicianexchange/MainActivity.kt
+++ app/src/main/java/com/adri/musicianexchange/MainActivity.kt
@@ -2,11 +2,15 @@ package com.adri.musicianexchange
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_main_Wee.setOnClickListener { Toast.makeText(this,"Soy un botón, hago cosas",Toast.LENGTH_SHORT) }
     }
 }
